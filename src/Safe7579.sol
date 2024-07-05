9bf62e744b87f64e09814d5b4fe78fa60ab60f7b
kopy-kat <kopp.konrad@gmail.com>
2024-07-05 23:23:35 +0200

    fix: typo

--- src/Safe7579.sol
+++ src/Safe7579.sol
@@ -470,7 +470,7 @@ contract Safe7579 is ISafe7579, SafeOp, SupportViewer, AccessControl, Initialize
     {
         if (moduleType == MODULE_TYPE_VALIDATOR) {
             // Safe7579 adapter allows for validator fallback to Safe's checkSignatures().
-            // It can thus be considered a valid validtor module
+            // It can thus be considered a valid validator module
             if (module == msg.sender) return true;
             return _isValidatorInstalled(module);
         } else if (moduleType == MODULE_TYPE_EXECUTOR) {
