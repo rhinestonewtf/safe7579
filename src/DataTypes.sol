10b4b3180b8d383fa6d1ce2fd39dd6df83887127
kopy-kat <kopp.konrad@gmail.com>
2024-11-12 21:26:23 +0700

    chore: unify pragma min

--- src/DataTypes.sol
+++ src/DataTypes.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.23;
 
 import { IERC7484 } from "./interfaces/IERC7484.sol";
 import { CallType } from "./lib/ModeLib.sol";
