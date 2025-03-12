c4e92c8d0cbb5dbdab4c5e7e1a88ad7cff3a9cbe
kopy-kat <kopp.konrad@gmail.com>
2025-03-12 11:29:24 +0000

    feat: add no launchpad test

--- test/flavors/NoLaunchpad.t.sol
+++ test/flavors/NoLaunchpad.t.sol
@@ -46,23 +46,23 @@ contract NoLaunchpad is Safe7579Test {
 
         uint256 saltNonce = 222;
 
-        // bytes memory deploymentData = abi.encodePacked(
-        //     safeProxyFactory.proxyCreationCode(), uint256(uint160(address(singleton)))
-        // );
-        //
-        // bytes32 salt = keccak256(abi.encodePacked(keccak256(initializer), saltNonce));
-        //
-        // bytes32 hash = keccak256(
-        //     abi.encodePacked(
-        //         bytes1(0xff), // prefix
-        //         address(0x104fBc016F4bb334D775a19E8A6510109AC63E00), // deployer address
-        //         salt, // salt
-        //         keccak256(deploymentData) // bytecode hash
-        //     )
-        // );
-        //
-        // address account = payable(address(uint160(uint256(hash))));
-        address account = address(0xB2BD76082593E35532472287D725bED7090De763);
+        bytes memory deploymentData = abi.encodePacked(
+            safeProxyFactory.proxyCreationCode(), uint256(uint160(address(singleton)))
+        );
+
+        bytes32 salt = keccak256(abi.encodePacked(keccak256(initializer), saltNonce));
+
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff), // prefix
+                address(safeProxyFactory), // deployer address
+                salt, // salt
+                keccak256(deploymentData) // bytecode hash
+            )
+        );
+
+        address account = payable(address(uint160(uint256(hash))));
+
         vm.deal(address(account), 1 ether);
         return (
             account,
@@ -105,6 +105,38 @@ contract NoLaunchpad is Safe7579Test {
 
         // Assert that the value was set ie that execution was successful
         assertTrue(target.value() == 1337);
+    }
+
+    function test_secondOp() public {
+        test_with4337();
+
+        // Create calldata for the account to execute
+        bytes memory setValueOnTarget = abi.encodeCall(MockTarget.set, 420);
+
+        // Encode the call into the calldata for the userOp
+        bytes memory userOpCalldata = abi.encodeCall(
+            IERC7579Account.execute,
+            (
+                ModeLib.encodeSimpleSingle(),
+                ExecutionLib.encodeSingle(address(target), uint256(0), setValueOnTarget)
+            )
+        );
+
+        (address account,) = makeSafeWithoutLaunchpad();
+
+        PackedUserOperation memory userOp =
+            getDefaultUserOp(address(account), address(defaultValidator));
+        userOp.callData = userOpCalldata;
+
+        // Create userOps array
+        PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
+        userOps[0] = userOp;
+
+        // Send the userOp to the entrypoint
+        entrypoint.handleOps(userOps, payable(address(0x69)));
+
+        // Assert that the value was set ie that execution was successful
+        assertTrue(target.value() == 420);
     }
 
     function test_execViaExecutor() public override { }
