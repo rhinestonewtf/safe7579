0a21cd0ac990bdc667e1623da354ff9636beb4d5
kopy-kat <kopp.konrad@gmail.com>
2025-04-21 15:02:27 +0200

    fix: artifact generation

--- build-artifacts.sh
+++ build-artifacts.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-
 # Check if a contract name is provided
 if [ $# -eq 0 ]; then
     echo "Please provide a contract name as an argument."
-    echo "Usage: $0 <ContractName>"
+    echo "Usage: $0 <ContractPath>"
     exit 1
 fi
 
-CONTRACT_NAME=$1
+CONTRACT_PATH=$1
+CONTRACT_NAME=$(basename "$CONTRACT_PATH" .sol)
 
 mkdir -p ./artifacts/$CONTRACT_NAME
-forge build $CONTRACT_NAME
+FOUNDRY_PROFILE=release forge build $CONTRACT_PATH
 cp ./out/$CONTRACT_NAME.sol/* ./artifacts/$CONTRACT_NAME/.
-forge verify-contract --show-standard-json-input $(cast address-zero) $CONTRACT_NAME > ./artifacts/$CONTRACT_NAME/verify.json
-
+forge verify-contract --show-standard-json-input $(cast address-zero) $CONTRACT_PATH:$CONTRACT_NAME > ./artifacts/$CONTRACT_NAME/verify.json
